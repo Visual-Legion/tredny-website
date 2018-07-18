95262b5d3e3cb4262cadaeb4b86316ae45300771
Ulysse <u.coates@gmail.com>
2018-07-18 13:50:37 +0100

    update

--- src/js/code/contact.js
+++ src/js/code/contact.js
@@ -108,53 +108,53 @@ $(() => {
                 $('.contact-msg').removeClass('error')
             }
             $('.contact-msg').html('<img src="http://mailgun.github.io/validator-demo/loading.gif" alt="Loading...">');
-            // $.ajax({
-            //     type: "GET",
-            //     url: 'http://apilayer.net/api/check',
-            //     data: {
-            //         access_key: 'c673ff585d5fa454aa388487d9b7ef7c',
-            //         email: email,
-            //         smtp: 1,
-            //         format: 0
-            //     },
-            //     dataType: "json",
-            //     crossDomain: true,
-            //     success: function(data, status_text) {
-            // maybe add txt for disposable
-            //         if (data['format_valid' && !data['disposable']) {
-            //             if (data['did_you_mean']) {
-            //$('.contact-msg').addClass('error')
-            //                 $('.contact-msg').html('Error, did you mean <em>' + data['did_you_mean'] + '</em>?');
-            //                 return false;
-            //             } else if (!data['mx_found']) {
-            //$('.contact-msg').addClass('error')
-            //                 $('.contact-msg').html('The entered mail address is invalid.');
-            //                 return false;
-            //             } else {
             $.ajax({
-                type: 'POST',
-                url: ajax_object.ajax_url,
-                data: $('.contact_form').serialize(),
-                dataType: 'json',
-                success: function(response) {
-                    if (response.status == 'success') {
-                        $('.contact_form')[0].reset();
+                type: "GET",
+                url: 'http://apilayer.net/api/check',
+                data: {
+                    access_key: 'c673ff585d5fa454aa388487d9b7ef7c',
+                    email: email,
+                    smtp: 1,
+                    format: 0
+                },
+                dataType: "json",
+                crossDomain: true,
+                success: function(data, status_text) {
+                    //maybe add txt for disposable
+                    if (data['format_valid'] && !data['disposable']) {
+                        if (data['did_you_mean']) {
+                            $('.contact-msg').addClass('error');
+                            $('.contact-msg').html('Error, did you mean <em>' + data['did_you_mean'] + '</em>?');
+                            return false;
+                        } else if (!data['mx_found']) {
+                            $('.contact-msg').addClass('error');
+                            $('.contact-msg').html('The entered mail address is invalid.');
+                            return false;
+                        } else {
+                            $.ajax({
+                                type: 'POST',
+                                url: ajax_object.ajax_url,
+                                data: $('.contact_form').serialize(),
+                                dataType: 'json',
+                                success: function(response) {
+                                    if (response.status == 'success') {
+                                        $('.contact_form')[0].reset();
+                                    }
+                                    $('.contact-msg').html(response.errmessage);
+                                }
+                            });
+                        }
+
+                    } else {
+                        $('.contact-msg').html('The entered mail address is invalid.');
+                        return false;
                     }
-                    $('.contact-msg').html(response.errmessage);
+                },
+                error: function(request, status_text, error) {
+                    $('.contact-msg').html('Error occurred, unable to validate your email address.');
+                    return false;
                 }
             });
-            // }
-
-        //         } else {
-        //             $('.contact-msg').html('The entered mail address is invalid.');
-        //             return false;
-        //         }
-        //     },
-        //     error: function(request, status_text, error) {
-        //         $('.contact-msg').html('Error occurred, unable to validate your email address.');
-        //         return false;
-        //     }
-        // });
         }
     });
 
